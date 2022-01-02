e9cf077951ffa2a9f02db61d7f6ea43d24eef796
koenigm-seattleu <koenigm@seattleu.edu>
2022-01-02 08:23:46 -0800

    Adding UTs for RoundEngineGameTests.cs

--- UnitTests/Engine/EngineGame/RoundEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/RoundEngineGameTests.cs
@@ -1,6 +1,11 @@
-﻿
+﻿using System.Linq;
+using System.Threading.Tasks;
+
 using NUnit.Framework;
+
 using Game.Engine.EngineGame;
+using Game.Models;
+using Game.ViewModels;
 
 namespace UnitTests.Engine.EngineGame
 {
@@ -44,5 +49,273 @@ namespace UnitTests.Engine.EngineGame
             Assert.IsNotNull(result);
         }
         #endregion Constructor
+
+        #region OrderPlayListByTurnOrder
+        [Test]
+        public void RoundEngine_OrderPlayerListByTurnOrder_Valid_Speed_Higher_Should_Be_Z()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.Round.OrderPlayerListByTurnOrder();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void RoundEngine_OrderPlayerListByTurnOrder_Valid_Level_Higher_Should_Be_Z()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.Round.OrderPlayerListByTurnOrder();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void RoundEngine_OrderPlayerListByTurnOrder_Valid_Experience_Higher_Should_Be_Z()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.Round.OrderPlayerListByTurnOrder();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void RoundEngine_OrderPlayerListByTurnOrder_Valid_ListOrder_Should_Be_1()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.Round.OrderPlayerListByTurnOrder();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void RoundEngine_OrderPlayerListByTurnOrder_Valid_Name_A_Z_Should_Be_Z()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.Round.OrderPlayerListByTurnOrder();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+        #endregion OrderPlayListByTurnOrder
+
+        #region GetItemFromPoolIfBetter
+
+        //[Test]
+        //public async Task RoundEngine_GetItemFromPoolIfBetter_InValid_Pool_Empty_Should_Fail()
+        //{
+        //    Engine.EngineSettings.MonsterList.Clear();
+
+        //    // Both need to be character to fall through to the Name Test
+        //    // Arrange
+        //    var Character = new CharacterModel
+        //    {
+        //        Speed = 20,
+        //        Level = 1,
+        //        CurrentHealth = 1,
+        //        ExperienceTotal = 1,
+        //        Name = "Z",
+        //        ListOrder = 1,
+        //        Guid = "me"
+        //    };
+
+        //    // Add each model here to warm up and load it.
+        //    _ = Game.Helpers.DataSetsHelper.WarmUp();
+
+        //    var item1 = new ItemModel { Attribute = AttributeEnum.Attack, Value = 1, Location = ItemLocationEnum.Head };
+        //    var item2 = new ItemModel { Attribute = AttributeEnum.Attack, Value = 20, Location = ItemLocationEnum.Head };
+
+        //    _ = await ItemIndexViewModel.Instance.CreateAsync(item1);
+        //    _ = await ItemIndexViewModel.Instance.CreateAsync(item2);
+
+        //    //Engine.EngineSettings.ItemPool.Add(item1);
+        //    //Engine.EngineSettings.ItemPool.Add(item2);
+
+        //    // Put the Item on the Character
+        //    _ = Character.AddItem(ItemLocationEnum.Head, item2.Id);
+
+        //    var CharacterPlayer = new PlayerInfoModel(Character);
+        //    Engine.EngineSettings.CharacterList.Clear();
+        //    Engine.EngineSettings.CharacterList.Add(new PlayerInfoModel(Character));
+
+        //    // Make the List
+        //    Engine.EngineSettings.PlayerList = Engine.Round.MakePlayerList();
+
+        //    // Act
+        //    var result = Engine.Round.GetItemFromPoolIfBetter(CharacterPlayer, ItemLocationEnum.Head);
+
+        //    // Reset
+
+        //    // Assert
+        //    Assert.AreEqual(false, result);
+        //}
+
+        #endregion GetItemFromPoolIfBetter
+
+        #region PickupItemsFromPool
+        //[Test]
+        //public void RoundEngine_PickupItemsFromPool_Valid_Default_Should_Pass()
+        //{
+        //    // Arrange
+        //    var Character = new CharacterModel
+        //    {
+        //        Speed = 20,
+        //        Level = 1,
+        //        CurrentHealth = 1,
+        //        ExperienceTotal = 1,
+        //        Name = "Z",
+        //        ListOrder = 1,
+        //        Guid = "me"
+        //    };
+
+        //    // Add each model here to warm up and load it.
+        //    _ = Game.Helpers.DataSetsHelper.WarmUp();
+
+        //    var CharacterPlayer = new PlayerInfoModel(Character);
+        //    Engine.EngineSettings.CharacterList.Clear();
+        //    Engine.EngineSettings.CharacterList.Add(new PlayerInfoModel(Character));
+
+        //    // Make the List
+        //    Engine.EngineSettings.PlayerList = Engine.Round.MakePlayerList();
+
+        //    // Act
+        //    var result = Engine.Round.PickupItemsFromPool(CharacterPlayer);
+
+        //    // Reset
+
+        //    // Assert
+        //    Assert.AreEqual(false, result);
+        //}
+        #endregion PickupItemsFromPool
+
+        #region EndRound
+        [Test]
+        public void RoundEngine_EndRound_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.Round.EndRound();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+        #endregion EndRound
+
+        #region RoundNextTurn
+        [Test]
+        public void RoundEngine_RoundNextTurn_Valid_No_Characters_Should_Return_GameOver()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.Round.RoundNextTurn();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(RoundEnum.Unknown, result);
+        }
+
+        [Test]
+        public void RoundEngine_RoundNextTurn_Valid_No_Monsters_Should_Return_NewRound()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.Round.RoundNextTurn();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(RoundEnum.Unknown, result);
+        }
+
+        [Test]
+        public void RoundEngine_RoundNextTurn_Valid_Characters_Monsters_Should_Return_NewRound()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.Round.RoundNextTurn();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(RoundEnum.Unknown, result);
+        }
+        #endregion RoundNextTurn
+
+        #region GetNextPlayerInList
+
+        [Test]
+        public void RoundEngine_GetNextPlayerInList_Valid_Sue_Should_Return_Monster()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.Round.GetNextPlayerInList();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void RoundEngine_GetNextPlayerInList_Valid_Monster_Should_Return_Mike()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.Round.GetNextPlayerInList();
+
+            // Reset
+
+
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+
+        #endregion GetNextPlayerInList
+
+        #region PlayerList
+        //[Test]
+        //public void RoundEngine_PlayerList_Valid_Default_Should_Pass()
+        //{
+        //    // Act
+        //    var result = Engine.Round.PlayerList();
+
+        //    // Reset
+
+        //    // Assert
+        //    Assert.AreEqual(false, result.Any());
+        //}
+        #endregion PlayerList
     }
 }
