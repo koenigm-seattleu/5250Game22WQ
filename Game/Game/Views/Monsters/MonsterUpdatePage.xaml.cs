36510845c2318e9055e4b702e194bdaf2fc5cdf2
koenigm-seattleu <koenigm@seattleu.edu>
2022-01-02 06:30:51 -0800

    Resetting Monster Pages for Class Start

--- Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -39,12 +39,6 @@ namespace Game.Views
 
             this.ViewModel.Title = "Update " + data.Title;
 
-            // Load the values for the Diffculty into the Picker
-            foreach (var item in DifficultyEnumHelper.GetListMonster)
-            {
-                DifficultyPicker.Items.Add(item);
-            }
-
             _ = UpdatePageBindingContext();
         }
 
@@ -63,165 +57,7 @@ namespace Game.Views
 
             ViewModel.Data.Difficulty = difficulty;
 
-            DifficultyPicker.SelectedItem = difficulty;
-            DifficultyPicker.SelectedIndex = DifficultyPicker.Items.IndexOf(DifficultyPicker.SelectedItem.ToString());
-
-            AddItemsToDisplay();
-
             return true;
-        }
-
-        /// <summary>
-        /// Show the Items the Character has
-        /// </summary>
-        public void AddItemsToDisplay()
-        {
-            var FlexList = ItemBox.Children.ToList();
-            foreach (var data in FlexList)
-            {
-                _ = ItemBox.Children.Remove(data);
-            }
-
-            ItemBox.Children.Add(GetItemToDisplay());
-        }
-
-        /// <summary>
-        /// The row selected from the list
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        public void OnPopupItemSelected(object sender, SelectedItemChangedEventArgs args)
-        {
-            ItemModel data = args.SelectedItem as ItemModel;
-            if (data == null)
-            {
-                return;
-            }
-
-            ViewModel.Data.UniqueItem = data.Id;
-
-            AddItemsToDisplay();
-
-            ClosePopup();
-        }
-
-        /// <summary>
-        /// Look up the Item to Display
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public StackLayout GetItemToDisplay()
-        {
-            var data = ViewModel.Data.GetItem(ViewModel.Data.UniqueItem);
-            if (data == null)
-            {
-                // Show the Default Icon for the Location
-                data = new ItemModel { Location = ItemLocationEnum.Unknown, ImageURI = "icon_cancel.png" };
-            }
-
-            // Hookup the Image Button to show the Item picture
-            var ItemButton = new ImageButton
-            {
-                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
-                Source = data.ImageURI
-            };
-
-            // Add a event to the user can click the item and see more
-            ItemButton.Clicked += (sender, args) => ShowPopup(data);
-
-            // Add the Display Text for the item
-            var ItemLabel = new Label
-            {
-                Text = "Unique Drop",
-                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
-                HorizontalOptions = LayoutOptions.Center,
-                HorizontalTextAlignment = TextAlignment.Center
-            };
-
-            // Put the Image Button and Text inside a layout
-            var ItemStack = new StackLayout
-            {
-                Padding = 3,
-                Style = (Style)Application.Current.Resources["ItemImageLabelBox"],
-                HorizontalOptions = LayoutOptions.Center,
-                Children = {
-                    ItemButton,
-                    ItemLabel
-                },
-            };
-
-            return ItemStack;
-        }
-
-        /// <summary>
-        /// Show the Popup for the Item
-        /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        public bool ShowPopup(ItemModel data)
-        {
-            PopupItemSelector.IsVisible = true;
-
-            // Make a fake item for None
-            var NoneItem = new ItemModel
-            {
-                Id = null, // will use null to clear the item
-                Guid = "None", // how to find this item amoung all of them
-                ImageURI = "icon_cancel.png",
-                Name = "None",
-                Description = "None"
-            };
-
-            List<ItemModel> itemList = new List<ItemModel>
-            {
-                NoneItem
-            };
-
-            // Add the rest of the items to the list
-            itemList.AddRange(ItemIndexViewModel.Instance.Dataset);
-
-            // Populate the list with the items
-            PopupLocationItemListView.ItemsSource = itemList;
-            return true;
-        }
-
-
-        /// <summary>
-        /// When the user clicks the close in the Popup
-        /// hide the view
-        /// show the scroll view
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void ClosePopup_Clicked(object sender, EventArgs e)
-        {
-            ClosePopup();
-        }
-
-        /// <summary>
-        /// Close the popup
-        /// </summary>
-        public void ClosePopup()
-        {
-            PopupItemSelector.IsVisible = false;
-        }
-
-        /// <summary>
-        /// The Level selected from the list
-        /// Need to recalculate Max Health
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        public void Difficulty_Changed(object sender, EventArgs args)
-        {
-            // Check for null, SelectedItem is not set when the control is created
-            if (DifficultyPicker.SelectedItem == null)
-            {
-                return;
-            }
-
-            // Change the Difficulty
-            ViewModel.Data.Difficulty = DifficultyEnumHelper.ConvertStringToEnum(DifficultyPicker.SelectedItem.ToString());
         }
 
         /// <summary>
@@ -252,105 +88,30 @@ namespace Game.Views
             _ = await Navigation.PopModalAsync();
         }
 
-        /// <summary>
-        /// Catch the change to the Stepper for Attack
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Attack_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            AttackValue.Text = String.Format("{0}", e.NewValue);
-        }
+        ///// <summary>
+        ///// Randomize the Monster, keep the level the same
+        ///// </summary>
+        ///// <returns></returns>
+        //public bool RandomizeMonster()
+        //{
+        //    // Randomize Name
+        //    ViewModel.Data.Name = RandomPlayerHelper.GetMonsterName();
+        //    ViewModel.Data.Description = RandomPlayerHelper.GetMonsterDescription();
 
-        /// <summary>
-        /// Catch the change to the Stepper for Defense
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Defense_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            DefenseValue.Text = String.Format("{0}", e.NewValue);
-        }
+        //    // Randomize the Attributes
+        //    ViewModel.Data.Attack = RandomPlayerHelper.GetAbilityValue();
+        //    ViewModel.Data.Speed = RandomPlayerHelper.GetAbilityValue();
+        //    ViewModel.Data.Defense = RandomPlayerHelper.GetAbilityValue();
 
-        /// <summary>
-        /// Catch the change to the Stepper for Speed
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Speed_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            SpeedValue.Text = String.Format("{0}", e.NewValue);
-        }
+        //    ViewModel.Data.Difficulty = RandomPlayerHelper.GetMonsterDifficultyValue();
 
-        /// <summary>
-        /// Randomize Monster Values and Items
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void RollDice_Clicked(object sender, EventArgs e)
-        {
-            _ = DiceAnimationHandeler();
+        //    ViewModel.Data.ImageURI = RandomPlayerHelper.GetMonsterImage();
 
-            _ = RandomizeMonster();
+        //    ViewModel.Data.UniqueItem = RandomPlayerHelper.GetMonsterUniqueItem();
 
-            return;
-        }
+        //    _ = UpdatePageBindingContext();
 
-        /// <summary>
-        /// Randomize the Monster, keep the level the same
-        /// </summary>
-        /// <returns></returns>
-        public bool RandomizeMonster()
-        {
-            // Randomize Name
-            ViewModel.Data.Name = RandomPlayerHelper.GetMonsterName();
-            ViewModel.Data.Description = RandomPlayerHelper.GetMonsterDescription();
-
-            // Randomize the Attributes
-            ViewModel.Data.Attack = RandomPlayerHelper.GetAbilityValue();
-            ViewModel.Data.Speed = RandomPlayerHelper.GetAbilityValue();
-            ViewModel.Data.Defense = RandomPlayerHelper.GetAbilityValue();
-
-            ViewModel.Data.Difficulty = RandomPlayerHelper.GetMonsterDifficultyValue();
-
-            ViewModel.Data.ImageURI = RandomPlayerHelper.GetMonsterImage();
-
-            ViewModel.Data.UniqueItem = RandomPlayerHelper.GetMonsterUniqueItem();
-
-            _ = UpdatePageBindingContext();
-
-            return true;
-        }
-
-        /// <summary>
-        /// Setup the Dice Animation
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public bool DiceAnimationHandeler()
-        {
-            // Animate the Rolling of the Dice
-            ImageButton image = RollDice;
-            uint duration = 1000;
-
-            var parentAnimation = new Animation();
-
-            // Grow the image Size
-            var scaleUpAnimation = new Animation(v => image.Scale = v, 1, 2, Easing.SpringIn);
-
-            // Spin the Image
-            var rotateAnimation = new Animation(v => image.Rotation = v, 0, 360);
-
-            // Shrink the Image
-            var scaleDownAnimation = new Animation(v => image.Scale = v, 2, 1, Easing.SpringOut);
-
-            parentAnimation.Add(0, 0.5, scaleUpAnimation);
-            parentAnimation.Add(0, 1, rotateAnimation);
-            parentAnimation.Add(0.5, 1, scaleDownAnimation);
-
-            parentAnimation.Commit(this, "ChildAnimations", 16, duration, null, null);
-
-            return true;
-        }
+        //    return true;
+        //}
     }
 }
