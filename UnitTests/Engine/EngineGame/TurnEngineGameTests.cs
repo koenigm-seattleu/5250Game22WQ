0e69dde2e67be723e718b913e8d3f2171e884c56
koenigm-seattleu <koenigm@seattleu.edu>
2022-01-03 12:38:14 -0800

    Add UT for CalculateExperience in TurnEngineGameTests.cs

--- UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
@@ -184,5 +184,21 @@ namespace UnitTests.Engine.EngineGame
             Assert.AreEqual(HitStatusEnum.Unknown, result);
         }
         #endregion BattleSettingsOverride
+
+        #region CalculateExperience
+        [Test]
+        public void RoundEngine_CalculateExperience_Valid_Default_Should_Pass()
+        {
+            // Arrange 
+
+            // Act
+            var result = Engine.Round.Turn.CalculateExperience(new PlayerInfoModel(), new PlayerInfoModel());
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+        #endregion CalculateExperience
     }
 }
