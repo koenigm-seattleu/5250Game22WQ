a8f6515f3b3baa8080e54bc0700d09d46279dd86
koenigm-seattleu <koenigm@seattleu.edu>
2022-01-03 12:32:37 -0800

    Added UT BattleSettingsOverrideHitStatusEnum for TurnEngineGameTests.cs

--- UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
@@ -152,5 +152,21 @@ namespace UnitTests.Engine.EngineGame
             Assert.AreEqual(false, result);
         }
         #endregion UseAbility
+
+        #region BattleSettingsOverrideHitStatusEnum
+        [Test]
+        public void RoundEngine_BattleSettingsOverrideHitStatusEnum_Valid_Default_Should_Pass()
+        {
+            // Arrange 
+
+            // Act
+            var result = Engine.Round.Turn.BattleSettingsOverrideHitStatusEnum(HitStatusEnum.Unknown);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(HitStatusEnum.Unknown, result);
+        }
+        #endregion BattleSettingsOverrideHitStatusEnum
     }
 }
