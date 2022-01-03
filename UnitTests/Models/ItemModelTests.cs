da83732646d2db78591a93c06b2666bba24850f3
koenigm-seattleu <koenigm@seattleu.edu>
2022-01-03 13:37:26 -0800

    Updated to use _= for return in in ItemModelTests.cs

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -151,14 +151,14 @@ namespace UnitTests.Models
             // Arrange
             var data = new ItemModel();
 
-            DiceHelper.EnableForcedRolls();
-            DiceHelper.SetForcedRollValue(1);
+            _ = DiceHelper.EnableForcedRolls();
+            _ = DiceHelper.SetForcedRollValue(1);
 
             // Act
             var result = data.ScaleLevel(1);
 
             // Reset
-            DiceHelper.DisableForcedRolls();
+            _ = DiceHelper.DisableForcedRolls();
 
             // Assert 
             Assert.AreEqual(1, result);
