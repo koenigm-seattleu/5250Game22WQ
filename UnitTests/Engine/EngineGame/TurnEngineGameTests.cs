4d77ea6f31baa419751b6014a8e28ce74f14a0e4
koenigm-seattleu <koenigm@seattleu.edu>
2022-01-03 12:52:39 -0800

    Added UT for TakeTurn to TurnEngineGameTests.cs

--- UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
@@ -312,5 +312,21 @@ namespace UnitTests.Engine.EngineGame
             Assert.AreEqual(false, result);
         }
         #endregion TargetDied
+
+        #region TakeTurn
+        [Test]
+        public void RoundEngine_TakeTurn_Valid_Default_Should_Pass()
+        {
+            // Arrange 
+
+            // Act
+            var result = Engine.Round.Turn.TakeTurn(new PlayerInfoModel());
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+        #endregion TakeTurn
     }
 }
