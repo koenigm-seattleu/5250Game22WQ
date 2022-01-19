fe8809a7a63f600c59e2b4530a432d632f30b4b9
koenigm-seattleu <koenigm@seattleu.edu>
2022-01-19 07:34:53 -0800

    Added CharacterIndexFlexPage, xaml, cs and UT to project

---
+++ Game/Game/Views/Characters/CharacterIndexFlexPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.ViewModels;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Index Page
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CharacterIndexFlexPage: ContentPage
+    {
+        // The view model, used for data binding
+        readonly CharacterIndexViewModel ViewModel = CharacterIndexViewModel.Instance;
+
+        public CharacterIndexFlexPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the CharacterIndexView Model
+        /// </summary>
+        public CharacterIndexFlexPage()
+        {
+            InitializeComponent();
+
+            BindingContext = ViewModel;
+        }
+
+    }
+}
---
+++ UnitTests/Views/Characters/CharacterIndexFlexPageTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+using Game;
+using Game.Views;
+using Game.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests.Views
+{
+    [TestFixture]
+    public class CharacterIndexFlexPageTests : CharacterIndexFlexPage
+    {
+        App app;
+        CharacterIndexFlexPage page;
+
+        public CharacterIndexFlexPageTests() : base(true) { }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new CharacterIndexFlexPage();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        [Test]
+        public void CharacterIndexFlexPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
