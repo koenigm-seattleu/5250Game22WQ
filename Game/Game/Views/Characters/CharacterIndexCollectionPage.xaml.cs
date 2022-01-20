903cb601a53f3dc66f93bf47ddb9558e1097add5
koenigm-seattleu <koenigm@seattleu.edu>
2022-01-19 17:56:56 -0800

    Added CharacterIndexCollectionPage.xaml, cs and UT to project

---
+++ Game/Game/Views/Characters/CharacterIndexCollectionPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Linq;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.ViewModels;
+using Game.Models;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Index Page
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CharacterIndexCollectionPage: ContentPage
+    {
+        // The view model, used for data binding
+        readonly CharacterIndexViewModel ViewModel = CharacterIndexViewModel.Instance;
+
+        public CharacterIndexCollectionPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the CharacterIndexView Model
+        /// </summary>
+        public CharacterIndexCollectionPage()
+        {
+            InitializeComponent();
+
+            BindingContext = ViewModel;
+        }
+
+        /// <summary>
+        /// The Selection
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var current = (e.CurrentSelection.FirstOrDefault() as CharacterModel);
+
+            await Navigation.PushAsync(new CharacterReadPage(new GenericViewModel<CharacterModel>(current)));
+
+            return;
+        }
+    }
+}
---
+++ UnitTests/Views/Characters/CharacterIndexCollectionPageTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+
+using Game;
+using Game.Views;
+using Game.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests.Views
+{
+    [TestFixture]
+    public class CharacterIndexCollectionPageTests : CharacterIndexCollectionPage
+    {
+        App app;
+        CharacterIndexCollectionPage page;
+
+        public CharacterIndexCollectionPageTests() : base(true) { }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new CharacterIndexCollectionPage();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        [Test]
+        public void CharacterIndexCollectionPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void CharacterIndexCollectionPage_OnCollectionViewSelectionChanged_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            var selectedCharacter = new CharacterModel();
+
+            CollectionView CharactersListView = (CollectionView)page.FindByName("CharactersListView");
+            
+            // Act
+
+            // Triggers the OnCollectionViewSelectionChanged
+            CharactersListView.SelectedItem = selectedCharacter;
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+    }
+}
