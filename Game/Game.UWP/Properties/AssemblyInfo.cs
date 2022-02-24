505281542b500360376d5f04879fa0078a54cb9c
koenigm-seattleu <koenigm@seattleu.edu>
2022-02-24 09:16:51 -0800

    Updated Assembly Information for Elves vs Trolls in AssemblyInfo.cs

--- Game/Game.UWP/Properties/AssemblyInfo.cs
+++ Game/Game.UWP/Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Game.UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Elves vs Trolls")]
+[assembly: AssemblyDescription("A Great game")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Game.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Seattle U")]
+[assembly: AssemblyProduct("ElvesTrolls")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("ElvesTrolls")]
 [assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: ComVisible(false)]
