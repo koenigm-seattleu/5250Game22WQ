7620646af47dd6b02f583cab1eff0afd4ad57b9b
koenigm-seattleu <koenigm@seattleu.edu>
2022-02-24 10:33:36 -0800

    update to use splashscreen in MainActivity.cs

--- Game/Game.Android/MainActivity.cs
+++ Game/Game.Android/MainActivity.cs
@@ -6,11 +6,12 @@ using Android.OS;
 
 namespace Game.Droid
 {
-    [Activity(Label = "Game", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "Game", Icon = "@mipmap/icon", Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            SetTheme(Resource.Style.MainTheme);
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
